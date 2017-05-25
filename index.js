89d743cdcb1f7c3d471685bb20e2090f04a13801
Youngbok Yoon <master@bluehack.net>
2017-05-26 00:37:38 +0900

    Remove useless cli commands

--- index.js
+++ index.js
@@ -17,20 +17,11 @@ prog
   .version(packageInfo.version)
   // the "login" command
   .command('login', 'Login to apiway.io')
-  .help(`OAuth login`)
+  .help(`Login with OAuth`)
   .alias('sign-in')
-  .argument('<oauth-provider>', 'OAuth provider', ["github"])
+  .option('--oauth <oauth-provider>', 'OAuth provider (default: github)', ["github"])
+  // .argument('<oauth-provider>', 'OAuth provider', ["github"])
 
-  .option('-n, --number <num>', 'Number of pizza', prog.INT, 1)
-  .option('-d, --discount <amount>', 'Discount offer', prog.FLOAT)
-  .option('-p, --pay-by <mean>', 'Pay by option')
-  .complete(function() {
-    return Promise.resolve(['cash', 'credit-card']);
-  })
-
-  // --extra will be auto-magicaly autocompleted by providing the user with 3 choices
-  .option('-e <ingredients>', 'Add extra ingredients', ['pepperoni', 'onion', 'cheese'])
-  .option('--add-ingredients <ingredients>', 'Add extra ingredients', prog.LIST)
   .action((args, options, logger) => {
     User.githubAuth(function(err, authed) {
       if (err) {
@@ -52,28 +43,5 @@ prog
     // logger.info("arguments: %j", args);
     // logger.info("options: %j", options);
   })
-
-  // the "return" command
-  .command('return', 'Return an order')
-  // <kind> will be auto-magicaly autocompleted by providing the user with 3 choices
-  .argument('<order-id>', 'Order id')
-  // enable auto-completion for <from-store> argument using a Promise
-  .complete(function() {
-    return Promise.resolve(['#82792', '#71727', '#526Z52']);
-  })
-  .argument('<to-store>', 'Store id')
-  .option('--ask-change <other-kind-pizza>', 'Ask for other kind of pizza')
-  .complete(function() {
-    return Promise.resolve(["margherita", "hawaiian", "fredo"]);
-  })
-  .option('--say-something <something>', 'Say something to the manager')
-  .action(function(args, options, logger) {
-    return Promise.resolve("wooooo").then(function (ret) {
-      logger.info("Command 'return' called with:");
-      logger.info("arguments: %j", args);
-      logger.info("options: %j", options);
-      logger.info("promise succeed with: %s", ret);
-    });
-  });
 
 prog.parse(process.argv);
